f508e596b135e7939e070bb34b6dda5667f33807
nanaka0012 <nanaka0012@yahoo.co.jp>
2021-02-17 03:53:48 +0900

    first release

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 var app = new Vue({
   el: '#app',
   data: {
-    isActive: 'profile'
-  }
+    isActive: 'profile',
+    works: [
+      {
+        title: "Hobeeeeee!!",
+        year: "2021/1",
+        description: "新しい趣味を見つけたい・広めたい人の願望を叶えるWebサービス。好きそうな趣味をつながりから探せることによって、サークルの新歓のように趣味を見つける・広めることができる。",
+        tags: ["Nuxt.js", "Firebase"],
+        image: "img/hobeeeeee.png",
+        link: "https://hobeeeeee.netlify.app/",
+      },
+    ],
+    games: [
+      {
+        title: "BlackJack!!",
+        year: "2020/10",
+        description: "サクッと遊べるトランプゲームのブラックジャック",
+        tags: ["C#","Altseed"],
+        image: "img/blackjack.png",
+      },
+      {
+        title: "たこシューティング",
+        year: "2020/3",
+        description: "向かってくるタコを避けつつ弾を発射してハイスコアを目指すゲーム",
+        tags: ["C#","Altseed"],
+        image: "img/tako.png",
+      },
+      {
+        title: "ちみちみざむらい",
+        year: "2019/8",
+        description: "侍を操作して、魑魅魍魎を蹴散らせ！シンプルな2Dアクション",
+        tags: ["C#", "Altseed"],
+        image: "img/ex2019.png",
+      },
+    ],
+  },
 })
